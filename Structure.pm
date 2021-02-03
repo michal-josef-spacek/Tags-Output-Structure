c5e8225f446b43cdd9c41a323269bf1f6458343d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-02-03 23:10:42 +0100

    Prepare for 0.07 version

--- Structure.pm
+++ Structure.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use Error::Pure qw(err);
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Flush.
 sub flush {
@@ -381,6 +381,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
